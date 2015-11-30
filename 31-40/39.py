f769358ff0dc7588f5809ab91dba67cc5273de55
luyishisi <543429245@qq.com>
2015-11-30 14:35:52 +0800

    35.36.py

--- 31-40/39.py
+++ 31-40/39.py
@@ -3,8 +3,5 @@
 给你一个字符串列表L，请用一行代码将列表所有元素拼接成一个字符串并输出。
 如L=['abc','d','efg'], 则输出abcdefg。
 '''
-print ''.join([str(x) for x in L])
-
 from __future__ import print_function
-print(*L,sep='')
-
+print(*L,sep=' ')
